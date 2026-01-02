c6d2d9afad917d59a6218af2fbccee4382e40c9a
meysaw <meysaw6@gmail.com>
2026-01-02 14:41:35 +0300

    comments...

--- run-mac.sh
+++ run-mac.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Make macOS binaries executable
 chmod +x bin/mac/*
 
-# Run fat JAR
 java -jar target/tusic-1.0-SNAPSHOT.jar
--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Make Linux binaries executable
 chmod +x bin/linux/*
 
-# Run fat JAR
 java -jar target/tusic-1.0-SNAPSHOT.jar
